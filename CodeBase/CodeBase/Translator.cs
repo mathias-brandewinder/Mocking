8e4fffd73116927d83a902c863bd3f95d0d9cf29
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2013-01-26 12:44:07 -0800

    Initial commit: code under test.

---
+++ CodeBase/CodeBase/Translator.cs
@@ -0,0 +1,39 @@
+﻿namespace CodeBase
+{
+    using System;
+
+    public class Translator
+    {
+        private readonly ILogger logger;
+        private readonly IService service;
+
+        public Translator(ILogger logger, IService service)
+        {
+            this.logger = logger;
+            this.service = service;
+        }
+
+        public string Translate(string input)
+        {
+            try
+            {
+                return this.service.Translate(input);
+            }
+            catch (Exception exception)
+            {
+                this.logger.Log(exception);
+                throw;
+            }
+        }
+    }
+
+    public interface ILogger
+    {
+        void Log(Exception exception);
+    }
+
+    public interface IService
+    {
+        string Translate(string input);
+    }
+}
