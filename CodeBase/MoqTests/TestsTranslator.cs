342abde2e9913c17900931cdfa65362d916e3494
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2013-01-26 17:58:47 -0800

    Added Moq example

---
+++ CodeBase/MoqTests/TestsTranslator.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoqTests
+{
+    using CodeBase;
+    using NUnit.Framework;
+    using Moq;
+
+    [TestFixture]
+    public class TestsTranslator
+    {
+        [Test]
+        public void Translate_Should_Return_Successful_Service_Response()
+        {
+            var input = "Hello";
+            var output = "Kitty";
+
+            var service = new Mock<IService>();
+            service.Setup(s => s.Translate(input)).Returns(output);
+
+            var logger = new Mock<ILogger>();
+
+            var translator = new Translator(logger.Object, service.Object);
+
+            var result = translator.Translate(input);
+
+            Assert.That(result, Is.EqualTo(output));
+        }
+
+        [Test]
+        public void When_Service_Fails_Translate_Should_Return_ErrorMessage()
+        {
+            var service = new Mock<IService>();
+            service.Setup(s => s.Translate(It.IsAny<string>())).Throws<Exception>();
+
+            var logger = new Mock<ILogger>();
+
+            var translator = new Translator(logger.Object, service.Object);
+
+            var result = translator.Translate("Hello");
+
+            Assert.That(result, Is.EqualTo(Translator.ErrorMessage));
+        }
+
+        [Test]
+        public void When_Service_Fails_Exception_Should_Be_Logged()
+        {
+            var error = new Exception();
+            var service = new Mock<IService>();
+            service.Setup(s => s.Translate(It.IsAny<string>())).Throws(error);
+
+            var logger = new Mock<ILogger>();
+
+            var translator = new Translator(logger.Object, service.Object);
+
+            translator.Translate("Hello");
+
+            logger.Verify(l => l.Log(error));
+        }
+    }
+}
