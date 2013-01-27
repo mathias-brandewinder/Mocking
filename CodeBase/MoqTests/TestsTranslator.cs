af1942d4049fc64c9b22301a8a4811dc67f09801
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2013-01-26 18:00:57 -0800

    Minor cleanup in Moq example

--- CodeBase/MoqTests/TestsTranslator.cs
+++ CodeBase/MoqTests/TestsTranslator.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MoqTests
+﻿namespace MoqTests
 {
+    using System;
     using CodeBase;
-    using NUnit.Framework;
     using Moq;
+    using NUnit.Framework;
 
     [TestFixture]
     public class TestsTranslator
